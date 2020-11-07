7e71e1280c8c473d4fbf1fcb3f8c7c9a9848f8a8
Joe <joedean83@gmail.com>
2020-11-07 15:04:57 -0500

    added paper shell

--- rwjf-analysis.R
+++ rwjf-analysis.R
@@ -2,14 +2,28 @@ library(tsne)
 #install.packages('factoextra')
 #library(factoextra)
 
-#t1 <- tsne(rwjf_M)
+t1 <- tsne(rwjf_M)
 
 
-rwjf_std <- (rwjf_M - apply(rwjf_M, 2, mean))/apply(rwjf_M, 2, sd) 
+rwjf_std <- scale(rwjf_M)
 
 
 p1 <- princomp(rwjf_std)
 
-plot(p1$sdev)
+screeplot(p1)
 
-cor(rwjf_M)
+
+
+plot(x=t1[,1], y=t1[,2])
+
+pca_data <- p1$scores
+
+combined <- cbind.data.frame(rwjf3[,c(1:7)], rwjf_M, pca_data)
+
+View(cor(combined[,-c(1:7)]))
+
+## What did we learn? 
+# 1st principal component .91 correlation with Poor or Fair Health and Frequent Physical distress - neg wealth and ed
+# 2nd principal component Rural/urban divide (high correlation with % rural and low with housing problems)
+# 3rd pc - uninsured children, uninsured, uninsured adults, hispanic, food access; negative cor with air pollution and insufficient sleep
+# 4th pc - 
